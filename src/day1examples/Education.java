c4a33676c0a472ca48c65a4fce9e42fce08ad50b
Java developer <chilakapatichowdary@gmail.com>
2019-08-27 20:33:20 -0700

    OSFST 116,117
    OSFST 116,117

---
+++ src/day1examples/Education.java
@@ -0,0 +1,14 @@
+package day1examples;
+
+public class Education {
+	public int a;
+
+	public Education(int a) {
+		super();
+		this.a = a;
+	}
+	
+public void display(){
+	System.out.println("the value is::"+a);
+}
+}
---
+++ src/day1examples/Employee.java
@@ -0,0 +1,42 @@
+package day1examples;
+
+public class Employee {
+	private String firstName;
+	 private String laststName;
+	 private long salary;
+	 
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLaststName() {
+		return laststName;
+	}
+
+
+	public void setLaststName(String laststName) {
+		this.laststName = laststName;
+	}
+
+
+	public long getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	
+
+
+}
